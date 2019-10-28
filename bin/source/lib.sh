6b9154843445abe72d404ce89f4b3ac4987abfdd
Sanhe Hu <husanhe@gmail.com>
2019-10-28 15:31:10 -0400

    add unix lib

---
+++ bin/source/lib.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# -*- coding: utf-8 -*-
+
+dir_here="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+source ${dir_here}/unix-lib.sh
---
+++ bin/source/unix-lib.sh
@@ -0,0 +1,150 @@
+#!/bin/bash
+# -*- coding: utf-8 -*-
+#
+# unix likes system (MacOS, Linux) shared library.
+#
+# will be used in
+#
+# - ``mac-setup-lib.sh``
+# - ``amazon-linux-setup-lib.sh``
+
+
+# test if a command is installed in your system
+# usage:
+#
+#   if this_command_exists "brew"; then ...
+this_command_exists() {
+    if [ -x "$(command -v $1)" ]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+
+display_command_install_status() {
+    if this_command_exists "$1"; then
+        print_colored_line $color_green "successfully installed $1"
+    else
+        print_colored_line $color_red "failed to install $1"
+    fi
+}
+
+
+try_install_this() {
+    if this_command_exists "$1"; then
+        print_colored_line $color_green "$1 is already installed."
+    else
+        print_colored_line $color_red "$1 is NOT installed!"
+        print_colored_line $color_cyan "start install $1 ..."
+        sudo yum install -y "$1"
+        display_command_install_status "$1"
+    fi
+}
+
+
+# if a file/dir exists, then remove it. do nothing if not exists.
+# usage:
+#
+#   rm_if_exists "/tmp/downloads"
+#   rm_if_exists "/tmp/test.txt"
+rm_if_exists() {
+    if [ -e $1 ]; then
+        if [ -d $1 ]; then
+            rm -r $1
+        elif [ -f $1 ]; then
+            rm $1
+        fi
+    fi
+}
+
+
+# if a dir not exists, then create it and all necessary parent dir.
+mkdir_if_not_exists() {
+    if ! [ -e $1 ]; then
+        mkdir -p $1
+    fi
+}
+
+
+# exit if not exists
+ensure_not_exists() {
+    if [ -e $1 ]; then
+        echo "${1} already exists!"
+        exit 1
+    fi
+}
+
+
+color_normal="\e[39m"
+color_black="\e[30m"
+color_red="\e[31m"
+color_green="\e[32m"
+color_yellow="\e[33m"
+color_blue="\e[34m"
+color_magenta="\e[35m"
+color_cyan="\e[36m"
+color_light_gray="\e[37m"
+color_dark_gray="\e[90m"
+color_light_red="\e[91m"
+color_light_green="\e[92m"
+color_light_yellow="\e[93m"
+color_light_blue="\e[94m"
+color_light_magenta="\e[95m"
+color_light_cyan="\e[96m"
+color_white="\e[97m"
+
+
+# render text in color
+colored_text() {
+    local tmp_color=$1
+    local tmp_text=$2
+    echo "${tmp_color}${tmp_text}${color_normal}"
+}
+
+
+# render a path into green if exists, red if not exists
+colored_path() {
+    local tmp_path=$1
+    if [ -e ${tmp_path} ]; then
+        colored_text "${color_green}" "${tmp_path}"
+    else
+        colored_text "${color_red}" "${tmp_path}"
+    fi
+}
+
+
+# print a line with color
+# usage:
+#
+#   print_colored_line $color_red "Warning!"
+print_colored_line() {
+    local tmp_color=$1
+    local tmp_text=$2
+    printf -- "$(colored_text "${tmp_color}" "${tmp_text}")\n"
+}
+
+
+# print title, and description, title is colored
+#
+# - <title>: <description>
+#
+# usage:
+#   print_colored_line $color_red "GitHub Url" "www.github.com"
+print_colored_ref_line() {
+    local tmp_title_color=$1
+    local tmp_title=$2
+    local tmp_description=$3
+    printf -- "- $(colored_text "${tmp_title_color}" "${tmp_title}"): ${tmp_description}\n"
+}
+
+
+# Add a line to a file, if exact line not exists
+# usage:
+#
+#   add_line_to 'echo "Welcome to Bash!"' ~/.bash_profile
+add_line_to() {
+    if ! grep -xq "$1" "$2"; then
+        echo "$1" >> "$2"
+    fi
+}
